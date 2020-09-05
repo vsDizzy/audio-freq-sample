2bf4ce103a762e75350e6780daa9c8c802ab4d65
Pavel <todizzy@gmail.com>
2020-09-05 23:36:53 +0300

    Bar

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
-const cnv = document.getElementById('cnv') as HTMLCanvasElement;
-const ctx = cnv.getContext('2d');
+const bar = document.getElementById('bar') as HTMLCanvasElement;
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(0, 0, 50, 200);
+drawBar();
+
+function drawBar() {
+  const ctx = bar.getContext('2d');
+
+  const grd = ctx.createLinearGradient(0, 0, 0, 32);
+  grd.addColorStop(0, 'red');
+  grd.addColorStop(0.5, 'yellow');
+  grd.addColorStop(1, 'green');
+
+  ctx.fillStyle = grd;
+  ctx.fillRect(0, 0, 8, 32);
+}
+
+const img = document.getElementById('img') as HTMLImageElement;
+img.src = bar.toDataURL();
