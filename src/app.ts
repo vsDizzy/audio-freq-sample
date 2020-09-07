ae56fd52acf5e7178232b3a074ef133e228b2db2
Pavel <todizzy@gmail.com>
2020-09-07 15:28:05 +0300

    Add mp3 tune

--- src/app.ts
+++ src/app.ts
@@ -16,36 +16,37 @@ ii.src = sData;
 const osc = document.getElementById('osc') as HTMLCanvasElement;
 const ctx = osc.getContext('2d');
 
-let an: AnalyserNode;
+const au = document.getElementById('audio') as HTMLAudioElement;
+au.addEventListener('play', (ev) => {
+  ac.resume();
+});
 
-function draw() {
-  const data = new Uint8Array(an.frequencyBinCount);
-  an.getByteFrequencyData(data);
+const ac = new AudioContext();
+ac.suspend().then(init);
 
-  ctx.clearRect(0, 0, osc.width, osc.height);
-  for (let i = 0; i < data.length; i++) {
-    const x = Math.floor(data[i] / 8);
-    const h = x * 8;
-    let nh = osc.height - h;
-    ctx.drawImage(ii, 0, nh, 12, h, 12 * i, nh, 12, h);
+function init() {
+  const an = ac.createAnalyser();
+  an.fftSize = 128;
+
+  const src = ac.createMediaElementSource(au);
+  src.connect(an);
+  const dn = ac.createMediaStreamDestination();
+  src.connect(ac.destination);
+
+  draw();
+
+  function draw() {
+    const data = new Uint8Array(an.frequencyBinCount);
+    an.getByteFrequencyData(data);
+
+    ctx.clearRect(0, 0, osc.width, osc.height);
+    for (let i = 0; i < data.length; i++) {
+      const x = Math.floor(data[i] / 8);
+      const h = x * 8;
+      let nh = osc.height - h;
+      ctx.drawImage(ii, 0, nh, 12, h, 12 * i, nh, 12, h);
+    }
+
+    requestAnimationFrame(draw);
   }
-
-  requestAnimationFrame(draw);
 }
-
-const m = navigator.getUserMedia(
-  { audio: true },
-  (ms) => {
-    const ac = new AudioContext();
-    an = ac.createAnalyser();
-    an.fftSize = 64;
-
-    const src = ac.createMediaStreamSource(ms);
-    src.connect(an);
-
-    draw();
-  },
-  (e) => {
-    console.error(e);
-  }
-);
